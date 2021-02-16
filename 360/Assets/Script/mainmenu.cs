8b96496dca39b3bb7d1de889e07e2afbbe6bede1
Syamsul <syamsulnizar.sn@gmail.com>
2021-02-16 20:09:16 +0700

    Change Scene

---
+++ 360/Assets/Script/mainmenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class mainmenu : MonoBehaviour
+{
+
+
+    public void GoToScene2()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    
+}
