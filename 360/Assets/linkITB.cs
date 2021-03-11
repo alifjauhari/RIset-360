39e900bc933352cf6edcc6857915b01e920da63a
Alif Kamal Jauhari <alifkamal@student.telkomuniversity.ac.id>
2021-03-11 18:08:15 +0700

    ---360 NEW---

---
+++ 360/Assets/linkITB.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class linkITB : MonoBehaviour 
+{
+
+	public void OpenLinkMuseum()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://goo.gl/maps/49Ehtg4B2vRazSqR9");
+		#endif
+	}
+
+
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+}
